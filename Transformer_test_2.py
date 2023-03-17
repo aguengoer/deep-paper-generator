b734e01070ec98c964e9bee0758fa57708973276
Arapsih Güngör <a.guengoer@austrian-standards.at>
2023-03-17 14:23:10 +0100

    working

--- Transformer_test_2.py
+++ Transformer_test_2.py
@@ -3,7 +3,6 @@ import random
 import torch
 import torchtext
 import spacy
-from sklearn.model_selection import train_test_split
 
 # Define the fields to be used for preprocessing
 SRC = torchtext.legacy.data.Field(tokenize='spacy', init_token='<sos>', eos_token='<eos>', lower=True)
@@ -17,28 +16,33 @@ def load_data(path):
     return data
 
 
-# Create the ArxivDataset class
 class ArxivDataset(torchtext.legacy.data.Dataset):
-    def __init__(self, data, fields, **kwargs):
-        examples = []
-        for row in data:
-            src = row['title']
-            trg = row['abstract']
-            examples.append(torchtext.legacy.data.Example.fromlist([src, trg], fields))
-        super(ArxivDataset, self).__init__(examples, fields, **kwargs)
+    def __init__(self, examples, fields):
+        super().__init__(examples, fields)
+
+    @classmethod
+    def from_raw_data(cls, raw_data, src_field, trg_field):
+        fields = [('title', src_field), ('categories', src_field), ('authors', src_field), ('abstract', trg_field)]
+        examples = [torchtext.legacy.data.Example.fromlist(
+            [item['title'], item['categories'], item['authors'], item['abstract']], fields) for item in raw_data]
+        return cls(examples, fields)
 
 
 # Create the Transformer model
 class TransformerModel(torch.nn.Module):
-    def __init__(self, input_dim, output_dim, hidden_dim, num_layers, num_heads, dropout):
+    def __init__(self, d_model, num_layers, num_heads, hidden_dim, dropout, input_dim):
         super().__init__()
 
-        self.transformer = torch.nn.Transformer(input_dim, output_dim, num_layers, num_heads, hidden_dim, dropout)
-        self.fc = torch.nn.Linear(output_dim, output_dim)
+        self.embedding = torch.nn.Embedding(input_dim, d_model)
+        self.transformer = torch.nn.Transformer(d_model, num_heads, num_layers, dim_feedforward=hidden_dim, dropout=dropout)
+        self.fc = torch.nn.Linear(d_model, input_dim)
 
     def forward(self, src, trg):
         # src shape: (src_len, batch_size)
         # trg shape: (trg_len, batch_size)
+
+        src = self.embedding(src)
+        trg = self.embedding(trg)
 
         # Pass the inputs through the transformer model
         output = self.transformer(src, trg)
@@ -48,7 +52,6 @@ class TransformerModel(torch.nn.Module):
 
         return output
 
-
 # Define the training function
 def train(model, iterator, optimizer, criterion, clip):
     model.train()
@@ -56,8 +59,8 @@ def train(model, iterator, optimizer, criterion, clip):
     epoch_loss = 0
 
     for i, batch in enumerate(iterator):
-        src = batch.src
-        trg = batch.trg
+        src = batch.title
+        trg = batch.abstract
 
         optimizer.zero_grad()
 
@@ -86,8 +89,8 @@ def evaluate(model, iterator, criterion):
 
     with torch.no_grad():
         for i, batch in enumerate(iterator):
-            src = batch.src
-            trg = batch.trg
+            src = batch.title
+            trg = batch.abstract
 
             output = model(src, trg[:-1])
             output_dim = output.shape[-1]
@@ -111,12 +114,12 @@ def main():
     data_path = 'test_data.json'
     data = load_data(data_path)
 
-    # Split the data into training and validation sets
-    train_data, val_data = train_test_split(data, test_size=0.2, random_state=1234)
+    # Split the dataset into training and validation sets
+    train_data, val_data = data[:int(len(data) * 0.8)], data[int(len(data) * 0.8):]
 
     # Create the dataset using the fields
-    train_dataset = ArxivDataset(train_data, fields=[('src', SRC), ('trg', TRG)])
-    val_dataset = ArxivDataset(val_data, fields=[('src', SRC), ('trg', TRG)])
+    train_dataset = ArxivDataset.from_raw_data(train_data, SRC, TRG)
+    val_dataset = ArxivDataset.from_raw_data(val_data, SRC, TRG)
 
     # Build the vocabulary for the fields
     SRC.build_vocab(train_dataset, min_freq=2)
@@ -129,18 +132,18 @@ def main():
     # Create the iterators for the training and validation sets
     train_iterator, val_iterator = torchtext.legacy.data.BucketIterator.splits((train_dataset, val_dataset),
                                                                                batch_size=BATCH_SIZE, device=device,
-                                                                               sort_key=lambda x: len(x.src))
+                                                                               sort_key=lambda x: len(x.title))
 
     # Define the hyperparameters for the model
-    INPUT_DIM = len(SRC.vocab)
-    OUTPUT_DIM = len(TRG.vocab)
-    HIDDEN_DIM = 256
+    D_MODEL = 256
+    HIDDEN_DIM = 512
     NUM_LAYERS = 2
     NUM_HEADS = 8
     DROPOUT = 0.1
 
     # Create the model
-    model = TransformerModel(INPUT_DIM, OUTPUT_DIM, HIDDEN_DIM, NUM_LAYERS, NUM_HEADS, DROPOUT).to(device)
+    input_dim = len(SRC.vocab)
+    model = TransformerModel(D_MODEL, NUM_LAYERS, NUM_HEADS, HIDDEN_DIM, DROPOUT, input_dim).to(device)
 
     # Define the loss function and optimizer
     criterion = torch.nn.CrossEntropyLoss(ignore_index=TRG.vocab.stoi['<pad>'])
